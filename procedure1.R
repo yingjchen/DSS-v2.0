dadc26f1958380ce01e6fbc0bcce6f2b85126677
Yingjia Chen <44263178+yingjchen@users.noreply.github.com>
2023-03-10 18:21:30 +0800

    Update procedure1.R

--- procedure1.R
+++ procedure1.R
@@ -1,65 +1,49 @@
 ###DSS 2.0, sDSS/zDSS/rDSS computations
 ###set up
 ##Install required libraries
-packages.required <- c("plotly", "scales", "parallel", "foreach", "gridExtra", "grid", "graphics", "gplots",
-                       "ggplot2", "raster", "xtable","Rcpp","dplyr", "drc", "caTools", "gsubfn", "gtools", "data.table", "doSNOW","stringr",
-                       "MESS", "reshape", "reshape2", "matrixStats")
+packages.required <- c("plotly", "scales", "parallel", "foreach", "gridExtra", "grid", "graphics", "gplots", "ggplot2",  "xtable","Rcpp","dplyr", "drc", "caTools", "gsubfn", "gtools", "data.table", "MESS", "reshape", "reshape2", "matrixStats", "egg", "pheatmap",  "svMisc", "BiocManager")
+packages.bio <- c("sva", "pcaMethods")
 packages.new <- packages.required[!(packages.required %in% installed.packages()[,"Package"])]
 if(length(packages.new)) install.packages(packages.new)
+if (!requireNamespace(packages.bio, quietly = TRUE))
+    BiocManager::install(packages.bio)
+
 
 ##load the packages
-lapply(packages.required, library, character.only = !0)
+lapply(packages.required, library, character.only = T)
+lapply(packages.bio, library, character.only = T)
 
 
-rm(list = ls()) # REMOVE IT, WHAT IF PEOPLE HAVE SOME IMPORTANT OBJECT IN THE ENV?
-setwd('D:/AML/DSS/') # ALSO THIS ONE
+path_to_working_directory <- '/path/to/working/directory/'
+setwd(dir = path_to_working_directory)
 
-#url_to_dsscalc <- 'https://github.com/xxx' 
-#source(url_to_dsscalc)
-source('./exampledata/github repo/DSS.R')
+download.file(url = 'https://github.com/yingjchen/DSS-v2.0/archive/refs/heads/main.zip', destfile = 'DSS-v2.0-main.zip')
+unzip('DSS-v2.0-main.zip') 
+setwd(dir = file.path(path_to_working_directory, 'DSS-v2.0-main'))
+
+source('./DSS.R')
+source('./HelperFunctions.R')
+
 
 ###1. calculate the DSS scores (DSS1, DSS2, DSS3) from the cell viability data
-##load the example data: the cell viability of 10 drugs were tested in 2 FIMM patient samples
-path_to_exampledata <- './exampledata/github repo/exampledata_procedure1_c1.csv'
-
+##load the example data: the cell viability of ~500 drugs were tested in 3 FIMM patient samples
+path_to_exampledata <- './exampleData_procedure1.csv'
 df_dose.responses <- read.csv(path_to_exampledata, header = T,sep = ",",check.names = F)
 
-colnames(df_dose.responses) <- c('drug', 'Concentration','Patient.num' ,'response.raw')
-df_dose.responses$ID <- paste0(df_dose.responses$drug,"_Patient ", df_dose.responses$Patient.num)
-# compute cell inhibition
-df_dose.responses$inhibition_percent <- 100 - df_dose.responses$response.raw*100  
-
+df_dose.responses <- DOSE_RESPONSE_PROCESS(df_dose.responses, viability = TRUE)
 df_dose.responses.grouped <- df_dose.responses %>% 
   group_by(ID, Patient.num, drug) %>% 
   summarise(conc = max(Concentration)) %>%
-  as.data.frame()  
+  as.data.frame()
 
 ##compute BREEZE drug sensitivity metrics
-iter <- nrow(df_dose.responses.grouped)
-list.DSS_metrics <- list()
-list.believe <- list()
-list.IC50 <- list()
-
-for(it in 1:iter){
-  df.DSS.ctx <- CALC_IC50_EC50_DSS(it, df_dose.responses.grouped, df_dose.responses, DSS_typ="2", readoutCTX = T)
-  list.believe[[it]] <- data.frame(believe_DSS=df.DSS.ctx[[3]])
-  list.DSS_metrics[[it]] <- data.frame(AUC=df.DSS.ctx[[1]]$AUC,DSS1=df.DSS.ctx[[5]],
-                                       DSS2=df.DSS.ctx[[6]],DSS3=df.DSS.ctx[[7]])
-  list.IC50[[it]] <- data.frame(IC50=df.DSS.ctx[[8]])
-  rm( df.DSS.ctx) # NO NEED TO REMOVE HERE, IT SHOULD BE REMOVED BY GC
-  svMisc::progress(it/iter*100)
-}
-
-df.believe <- bind_rows(list.believe)
-df.DSS_metrics <- bind_rows(list.DSS_metrics)
-df.IC50_metrics <- bind_rows(list.IC50)
-df_scores <- cbind( df_dose.responses.grouped, df.DSS_metrics, df.IC50_metrics, df.believe)
+df.metrics <- CALC_METRICS(df_dose.responses,df_dose.responses.grouped)
 
 ##here we select DSS2 as patient DSS
-patients.dss <- as.data.frame(acast(df_scores,df_scores$Patient.num ~ df_scores$drug , value.var  = 'DSS2'))
+patients.dss <- as.data.frame(acast(df.metrics,df.metrics$Patient.num ~ df.metrics$drug , value.var  = 'DSS2'))
 
 ##save DSS scores and other metrics
-#write.table(df_scores, file = './Results_exampleData_procedure1.csv',sep = ',',append=F,row.names = F,col.names = T)
+#write.table(df.metrics, file = './Results_exampleData_procedure1.csv',sep = ',',append=F,row.names = F,col.names = T)
 
 
 ###2. load DSS data of 10 FIMM controls (FO5A plate)
@@ -70,30 +54,13 @@ controls.dss <- read.csv(path_to_controldss, header = T, sep = '\t', row.names =
 controls.summary <- as.data.frame(rbind(colMeans(as.matrix(controls.dss)),colSds(as.matrix(controls.dss)),colMedians(as.matrix(controls.dss)), colMads(as.matrix(controls.dss))))
 rownames(controls.summary ) <- c('mean', 'sd', 'median', 'mad')
 
-path_to_controldss <- './exampledata/github repo/File_1_Drugname_response_DSS_10Healthy.txt'
-controls.dss <- read.csv(path_to_controldss, header = T, sep = '\t', row.names = 1,stringsAsFactors = F, check.names = F)
-
-controls.summary <- as.data.frame(rbind(colMeans(as.matrix(controls.dss)),colSds(as.matrix(controls.dss)),colMedians(as.matrix(controls.dss)), colMads(as.matrix(controls.dss))))
-rownames(controls.summary ) <- c('mean', 'sd', 'median', 'mad')
 
 
 ###3.search the drugs of interest from the drug library (527 drugs) 
 ##3.1 map the drugs with drug synonyms
-path_to_druglibrary <- './exampledata/github repo/File_2_Drugname_library_527D.txt'
+path_to_druglibrary <- './controls/File_2_Drugname_library_527D.txt'
 df_drug.library <- read.csv(path_to_druglibrary, header = T, sep = '\t', row.names = NULL,stringsAsFactors = F, check.names = F)
-
-colnames_drug <- toupper(colnames(patients.dss))
-patients.dss.map <- patients.dss
-
-for (i in df_drug.library$drug){
-  drugsets = as.character(df_drug.library[df_drug.library$drug == i, "synonyms"])
-  drugsynonyms = unique(unlist(strsplit(drugsets,";")))
-  patients.dss.map['drugname', colnames_drug %in% drugsynonyms] = as.character(i)
-}
-
-##filter out unmatched drugs
-patients.dss <- patients.dss[, !(is.na(patients.dss.map['drugname',]))]
-colnames(patients.dss) <- patients.dss.map['drugname',]
+patients.dss <- DRUG_FILTER_SYNONYMS(patients.dss, df_drug.library)
 
 ##the drugs can also may be mapped with InChIKey from the drug library
 
@@ -114,3 +81,17 @@ write.table(patients.dss, file = './Results_exampledata_DSS_procedure1.txt',sep 
 write.table(patients.sdss, file = './Results_exampledata_sDSS_procedure1.txt',sep = '\t',append=F,row.names = T,col.names = T)
 write.table(patients.zdss, file = './Results_exampledata_zDSS_procedure1.txt',sep = '\t',append=F,row.names = T,col.names = T)
 write.table(patients.rdss, file = './Results_exampledata_rDSS_procedure1.txt',sep = '\t',append=F,row.names = T,col.names = T)
+
+
+###5. Plotting the drug response distributions (optional)
+sample_id <- 'AML_013_01'
+sample_dss <- SAMPLE_DSS_CONCAT(sample_id = sample_id)
+p1 <- ggplot(sample_dss) +  
+  geom_histogram(aes( y = after_stat(density), fill= drugclass, x = DSS), color = "black", alpha = .6, position="identity", show.legend = T) +
+  labs(title = sample_id,  x="DSS", y = "Density") + theme_classic()
+p2 <- ggplot(sample_dss, aes(y = drugclass, x = DSS, fill= drugclass)) +
+  geom_boxplot(aes(y = drugclass, x = DSS, fill = drugclass), outlier.shape = NA, alpha = .6,  colour = "black", show.legend = F) +
+  geom_jitter(aes(color = drugclass), size = 3, alpha = .8,  show.legend = F)+
+  theme_classic() 
+p3 <- ggarrange(p1, p2, nrow = 2)
+ggsave("./example_DSS_distribution.pdf", p3, height = 10,width = 10)
